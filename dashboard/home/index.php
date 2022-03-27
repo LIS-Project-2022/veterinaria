0fd406f75806b6cec6bdb22802c5852bb1868b87
RubidiaPaz <rubigarcia190@gmail.com>
2022-03-26 21:32:23 -0600

    correccion
    corrección en inicio

--- dashboard/home/index.php
+++ dashboard/home/index.php
@@ -1,6 +1,6 @@
 <?php
     require_once('../../app/views/template/page.class.php');
-    Page::templateHeader('Inicio');
+    Page::templateHeader('Bienvenido');
     require_once('../../app/views/home/index.php');
     Page::templateFooter();
 ?>
